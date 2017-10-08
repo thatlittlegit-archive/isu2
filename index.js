61278f50aada6f531a2e8683216970afab831d24
thatlittlegit <wapidstyle@live.ca>
2017-10-08 14:08:38 -0400

    Add a bunch of stuff and make it more functional

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 import Moon from 'moonjs';
 import domLoaded from 'dom-loaded';
 
-domLoaded().then(() => {
+window.app = null;
+
+Moon.directive('selectedtab', (el, val) => {
+	el.classList.remove('is-active', 'nvm');
+	el.classList.add(app.get('helptopic') === val ? 'is-active' : 'nvm');
+	el.onclick = () => {
+		console.log('CLICK');
+		app.set('helptopic', val);
+		app.build();
+	};
+});
+
+domLoaded.then(() => {
+	console.log('Initializing');
+	
+	app = new Moon({
+		el: '#app',
+		data: {
+			main: true,
+			help: false,
+			helptopic: 0
+		},
+		methods: {
+			toggle() {
+				new Promise(() => {
+					document.getElementsByTagName('html')[0].style.backgroundColor = !this.get('main') ? 'black' : 'inherit';
+				});
+				this.set('main', !this.get('main'));
+				this.set('help', !this.get('help'));
+			}
+		}
+	});
 });
